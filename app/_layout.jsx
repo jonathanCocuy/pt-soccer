e3f04ac098ad01ace0a35c1e963d2537d21fd67f
jonathanCocuy <jonathancp145@gmail.com>
2026-05-19 23:18:44 -0500

    learn(Stack, Slot): Stack and Slot practice

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from 'expo-router';
+import { View, StyleSheet, Text } from 'react-native'
 
 // Mantiene la pantalla de carga (Splash Screen) visible mientras cargan los assets
 SplashScreen.preventAutoHideAsync();
@@ -23,16 +24,46 @@ export default function RootLayout() {
     }
 
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#ffffff',
-                },
-                headerTitleStyle: {
-                    fontFamily: 'Inter-Bold',
-                },
-                headerTintColor: '#000000',
-            }}
-        />
+        // Stack just should be use in the layout component.
+        <View style={styles.container}>
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#FFF',
+                    },
+                    headerTitleStyle: {
+                        fontFamily: 'Inter-Bold',
+                    },
+                    headerTintColor: '#000',
+                }}
+            >
+                <Stack.Screen 
+                    name='index'
+                    options={{ title: 'This is the main screen' }}
+                />
+                <Stack.Screen 
+                    name='about'
+                    options={{ title: 'This is about' }}
+                />
+            </Stack>
+            <View style={styles.content}>
+                <Text style={styles.text}>This text will always appear.</Text>
+            </View>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    content: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    text: {
+        color: '#000'
+    }
+})
